78aa969eab6fc372aa884b7f0ba6a53dda38b429
civildinesh04 <74288869+civildinesh04@users.noreply.github.com>
2022-02-03 20:10:14 +0530

    Hello World Jenkins

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World Jenkins!"); 
         System.out.println("This is the first java program");
     }
 }
